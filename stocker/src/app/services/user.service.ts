2733f6dde051600599b8113d35330574b0508034
patrikpasztor <patrickhu97@gmail.com>
2020-10-24 16:34:23 +0200

    unusedBalance

--- stocker/src/app/services/user.service.ts
+++ stocker/src/app/services/user.service.ts
@@ -9,6 +9,7 @@ export class UserService {
   testUser: User = {
     name : 'paspat',
     balance: 12000,
+    unusedBalance: 3000,
     ownedStocks : [{symbol: 'A', buyingPrice: 10, currentPrice: 11, count: 1.5}, {symbol: 'MSFT', buyingPrice: 20, currentPrice: 15, count:10},{symbol: 'AAPL', buyingPrice: 100, currentPrice: 111, count: 5},],
     watchlistedStocks: [{symbol: 'ACCD', buyingPrice: 10, currentPrice: 11, count: 0}, {symbol: 'MSFT', buyingPrice: 20, currentPrice: 15, count: 0},{symbol: 'ADMP', buyingPrice: 100, currentPrice: 111, count: 0},]
   }
