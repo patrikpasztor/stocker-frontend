c0148766b2c23b75fbdf341acc9685f35e674128
patrikpasztor <patrickhu97@gmail.com>
2020-10-24 13:12:18 +0200

    getStockQuote call in stock service

--- stocker/src/app/services/stock.service.ts
+++ stocker/src/app/services/stock.service.ts
@@ -2,20 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { IStockSymbol } from '../models/stocksymbol';
+import { IStockQuote } from '../models/stockQuote';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
 
-  static readonly apiKey : string = 'bu4u4kf48v6sjdfq4b2g';
+  static readonly apiKey : string = '&token=bu4u4kf48v6sjdfq4b2g';
   static readonly baseUrl : string = 'https://finnhub.io/api/v1';
   static readonly listEndpoint : string = '/stock/symbol?exchange=US';
+  static readonly quoteEndpoint: string = '/quote/symbol='
 
   constructor(private http: HttpClient) { }
 
   listStocks(): Observable<IStockSymbol[]>{
-    return this.http.get<IStockSymbol[]>(StockService.baseUrl + StockService.listEndpoint + '&token=' + StockService.apiKey);
+    return this.http.get<IStockSymbol[]>(StockService.baseUrl + StockService.listEndpoint + StockService.apiKey);
+  }
+
+  getStockInfo(symbol: string): Observable<IStockQuote[]>{
+    return this.http.get<IStockQuote[]>(StockService.baseUrl + StockService.quoteEndpoint + symbol + StockService.apiKey);
   }
   
 }
